6d5247897ef7f798047440f618c3495741b946e8
fomotoshi <fomotoshi@ya.ru>
2022-02-26 20:33:31 +0800

    fix config download path for selenium

--- src/config.rs
+++ src/config.rs
@@ -7,7 +7,7 @@ pub fn get_browser_settings() -> Result<FirefoxPreferences> {
     let mut preferences = FirefoxPreferences::new();
     preferences.set("browser.download.folderList", 2u64)?;
     preferences.set("browser.download.manager.showWhenStarting", false)?;
-    preferences.set("browser.download.dir", "/screenshots")?;
+    preferences.set("browser.download.dir", "/home/seluser/screenshots")?;
     preferences.set("browser.helperApps.neverAsk.saveToDisk", "image/png")?;
 
     Ok(preferences)
