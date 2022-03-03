ef2bcf2995ad0fccbc2057ce04894b1c472099bd
fomotoshi <fomotoshi@ya.ru>
2022-03-03 18:20:16 +0800

    add Ipfs and headless mode

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 
 use actix_web::web;
+use ipfs_api_backend_hyper::IpfsClient;
 use thirtyfour::WebDriver;
 
 use crate::{config, routes, Result};
@@ -8,13 +9,17 @@ use crate::{config, routes, Result};
 pub fn run(
     listener: TcpListener,
     web_driver: WebDriver,
+    ipfs: IpfsClient,
 ) -> std::io::Result<actix_web::dev::Server> {
     let web_driver = web::Data::new(web_driver);
+    let ipfs = web::Data::new(ipfs);
+
     let server = actix_web::HttpServer::new(move || {
         actix_web::App::new()
             .route("/health_check", web::get().to(routes::health_check))
             .route("/save_png", web::get().to(routes::save_png))
             .app_data(web_driver.clone())
+            .app_data(ipfs.clone())
     })
     .listen(listener)?
     .run();
@@ -22,10 +27,13 @@ pub fn run(
     Ok(server)
 }
 
-pub async fn get_web_driver(address: &str) -> Result<WebDriver> {
-    let browser_settings = config::get_browser_settings()?;
+pub async fn get_web_driver(address: &str, headless: bool) -> Result<WebDriver> {
+    let browser_settings = config::set_up_browser()?;
     let mut capabilities = thirtyfour::DesiredCapabilities::firefox();
     capabilities.set_preferences(browser_settings)?;
+    if headless {
+        capabilities.set_headless()?;
+    }
     let driver = thirtyfour::WebDriver::new(address, &capabilities).await?;
 
     Ok(driver)
