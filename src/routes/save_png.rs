01df076e41e8e33ae4ac7d11f00db1cdea971212
fomotoshi <fomotoshi@ya.ru>
2022-03-03 18:19:10 +0800

    implement saving canvas and adding it to ipfs

--- src/routes/save_png.rs
+++ src/routes/save_png.rs
@@ -1,31 +1,31 @@
-use std::path::Path;
+use std::io::Cursor;
+use std::time::Duration;
 
 use actix_web::{web, HttpResponse};
-use thirtyfour::prelude::ElementWaitable;
+use ipfs_api_backend_hyper::{IpfsApi, IpfsClient};
 use thirtyfour::{By, WebDriver};
+use tokio::time::sleep;
 
-pub async fn save_png(driver: web::Data<WebDriver>) -> HttpResponse {
-    driver
-        .get("http://constructor")
-        .await
-        .expect("Couldn't get url");
+use crate::config::get_config;
 
-    let element = driver
+pub async fn save_png(driver: web::Data<WebDriver>, ipfs: web::Data<IpfsClient>) -> HttpResponse {
+    let constructor_url = &get_config().await.constructor.url();
+    driver.get(constructor_url).await.expect("Couldn't get url");
+
+    while driver.find_element(By::Id("download")).await.is_err() {
+        sleep(Duration::from_millis(100)).await;
+    }
+
+    let canvas = driver
         .find_element(By::ClassName("threejs-container"))
         .await
         .expect("Couldn't find element");
-
-    element
-        .wait_until()
-        .displayed()
+    let screenshot = canvas
+        .screenshot_as_png()
         .await
-        .expect("Error occurs while waiting element");
-
-    let file_path = Path::new("/app/screenshots/screen.png");
-    element
-        .screenshot(file_path)
-        .await
-        .expect("Couldn't save screenshot");
-
+        .expect("Couldn't capture screenshot");
+    let data = Cursor::new(screenshot);
+    let res = ipfs.add(data).await.expect("Couldn't add to ipfs data");
+    dbg!(res);
     HttpResponse::Ok().finish()
 }
