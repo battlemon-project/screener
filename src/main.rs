1ed7d0676c356bfc74212823cd9e328280fa3dd6
ChzenChzen <fedorovdanila@me.com>
2022-04-12 17:17:26 +0800

    added tracing to main

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,17 @@ use battlemon_screener::{config, startup, telemetry};
 async fn main() -> std::io::Result<()> {
     let subscriber = telemetry::get_subscriber("battlemon_screener".into(), "info".into());
     telemetry::init_subscriber(subscriber);
+    tracing::info!("Loading config.");
     let config = config::get_config().await;
+    tracing::info!("Bind listener.");
     let listener =
         std::net::TcpListener::bind(config.application.address()).expect("Couldn't bind address");
-    let web_driver = startup::get_web_driver(&config.webdriver.url(), config.webdriver.headless())
-        .await
-        .expect("Couldn't get web driver");
+    tracing::info!("Connect to webdriver");
+    let web_driver =
+        startup::connect_web_driver(&config.webdriver.url(), config.webdriver.headless())
+            .await
+            .expect("Couldn't connect to webdriver");
+    tracing::info!("Connect to IPFS.");
     let ipfs = ipfs_api_backend_hyper::IpfsClient::from_str(&config.ipfs.url())
         .expect("Couldn't connect to IPFS");
 
