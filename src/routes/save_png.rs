a9a5f54913b65051e787a1b0baa822b14f00b46c
ChzenChzen <fedorovdanila@me.com>
2022-04-12 17:51:22 +0800

    add tracing for sget_png_and_push_ipf function

--- src/routes/save_png.rs
+++ src/routes/save_png.rs
@@ -1,26 +1,30 @@
 use std::io::Cursor;
 use std::time::Duration;
 
-use actix_web::{web, HttpRequest, HttpResponse};
+use actix_web::{HttpRequest, HttpResponse, web};
 use ipfs_api_backend_hyper::{IpfsApi, IpfsClient};
 use thirtyfour::{By, WebDriver};
 use tokio::time::sleep;
 
 use crate::config::get_config;
 
-pub async fn save_png(
+#[tracing::instrument(name = "Handling save_png", skip(driver, ipfs))]
+pub async fn get_png_and_push_ipfs(
+    req: HttpRequest,
     driver: web::Data<WebDriver>,
     ipfs: web::Data<IpfsClient>,
-    req: HttpRequest,
 ) -> HttpResponse {
     let constructor_url = &get_config().await.constructor.url();
     let url = format!("{}/{}", constructor_url, req.query_string());
+    tracing::info!("Url: {}", url);
     driver.get(url).await.expect("Couldn't get url");
 
+    tracing::info!("Finding element with id download.");
     while driver.find_element(By::Id("download")).await.is_err() {
         sleep(Duration::from_millis(100)).await;
     }
 
+    tracing::info!("Getting picture's bytes");
     let canvas = driver
         .find_element(By::ClassName("threejs-container"))
         .await
@@ -30,6 +34,8 @@ pub async fn save_png(
         .await
         .expect("Couldn't capture screenshot");
     let data = Cursor::new(screenshot);
+
+    tracing::info!("Adding the picture to IPFS");
     let res = ipfs.add(data).await.expect("Couldn't add to ipfs data");
     let json = serde_json::json!({
         "hash": res.hash,
