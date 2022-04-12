821ea91f23157e386a600fcc5a883138b687114c
ChzenChzen <fedorovdanila@me.com>
2022-04-12 18:51:30 +0800

    get rid of redundant traces

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,10 @@ use battlemon_screener::{config, startup, telemetry};
 async fn main() -> std::io::Result<()> {
     let subscriber = telemetry::get_subscriber("battlemon_screener".into(), "info".into());
     telemetry::init_subscriber(subscriber);
-    tracing::info!("Loading config.");
     let config = config::get_config().await;
     tracing::info!("Bind listener.");
     let listener =
         std::net::TcpListener::bind(config.application.address()).expect("Couldn't bind address");
-    tracing::info!("Connect to webdriver");
     let web_driver =
         startup::connect_web_driver(&config.webdriver.url(), config.webdriver.headless())
             .await
